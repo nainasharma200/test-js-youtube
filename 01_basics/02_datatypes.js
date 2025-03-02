52e6e02ce0ca7cd6b3ea0610cd9d7557e2bcffaa
nainasharma200 <162029656+nainasharma200@users.noreply.github.com>
2025-03-02 17:36:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+// code readability should be high
+console.log("Naina")
+
+let name = "naina"
+let age = 3
+let isloggedIn = false
+let state = null 
+
+// number => 2 to power 53
+// bigint 
+//string => ""
+//boolean => true/false
+//undefined =>
+    //symbol => unique 
+
+//object 
+
+console.log(typeof undefined); //undefiend
+console.log(typeof null); //object
+
